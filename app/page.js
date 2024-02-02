d7c8dcaea492fb0c268dc0791fca041bbc8e56ea
Jose Ramon Alvarez <joraalgo@gmail.com>
2024-02-02 16:49:22 +0100

    eslint

--- app/page.js
+++ app/page.js
@@ -202,7 +202,7 @@ export default function Home () {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <span className='font-bold text-xl'>TEAM GENERATOR</span>
         </p>
-        <div className="flex flex-row items-center fixed bottom-0 left-0 h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="flex flex-row items-center fixed bottom-0 left-0 h-48 w-full justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
 
           <div className='mr-1'>By </div><div><span className='font-bold' style={{ fontSize: 30, fontFamily: 'system-ui' }}>Hoserpa</span></div>
 
