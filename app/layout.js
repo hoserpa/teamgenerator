6fe74df6daca07f46a050251facef3230462a3a5
hoserpa <joraalgo@gmail.com>
2025-10-02 12:08:54 +0200

    Arreglo para dev

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-import '../public/globals.css'
+import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: 'export',
-  basePath: "/teamgenerator",
-  assetPrefix: "/teamgenerator/"
+  output: 'export'
 }
 
 module.exports = nextConfig
